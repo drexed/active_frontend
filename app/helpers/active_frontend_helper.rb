78803b853c45af5a697ca21925845c6037dbfdd6
Juan Gomez <j.gomez@drexed.com>
2016-09-18 00:53:02 -0400

    updates

--- app/helpers/active_frontend_helper.rb
+++ app/helpers/active_frontend_helper.rb
@@ -3,26 +3,26 @@ module ActiveFrontendHelper
   def colors(opacity = 1)
     {
       transparent: { rgba: "rgba(0,0,0,0)", hex: '#FFFFFF' },
-      dark_black: { rgba: "rgba(31,45,61,#{opacity})", hex: '#1F2D3D' },
-      black: { rgba: "rgba(39,52,72,#{opacity})", hex: '#273448' },
-      light_black: { rgba: "rgba(50,65,86,#{opacity})", hex: '#344358' },
-      dark_gray: { rgba: "rgba(104,120,143,#{opacity})",  hex: '#68788F' },
-      gray: { rgba: "rgba(132,146,166,#{opacity})", hex: '#8492A6' },
-      light_gray: { rgba: "rgba(155,167,186,#{opacity})", hex: '#9BA7BA' },
+      dark_black: { rgba: "rgba(44,50,62,#{opacity})", hex: '#2C323E' },
+      black: { rgba: "rgba(52,59,74,#{opacity})", hex: '#343B4A' },
+      light_black: { rgba: "rgba(60,68,86,#{opacity})", hex: '#3C4456' },
+      dark_gray: { rgba: "rgba(143,157,178,#{opacity})",  hex: '#8F9DB2' },
+      gray: { rgba: "rgba(155,167,186,#{opacity})", hex: '#9BA7BA' },
+      light_gray: { rgba: "rgba(167,177,194,#{opacity})", hex: '#A7B1C2' },
       dark_haze: { rgba: "rgba(227,232,240,#{opacity})", hex: '#E0E5ED' },
       haze: { rgba: "rgba(239,242,248,#{opacity})", hex: '#ECEFF5' },
       light_haze: { rgba: "rgba(249,250,252,#{opacity})", hex: '#F7F8FA' },
       white: { rgba: "rgba(255,255,255,#{opacity})", hex: '#FFFFFF' },
-      lime: { rgba: "rgba(171,208,38,#{opacity})", hex: '#ABD026' },
-      green: { rgba: "rgba(114,201,20,#{opacity})", hex: '#72C914' },
-      teal: { rgba: "rgba(50,201,234,#{opacity})", hex: '#32C9EA' },
-      blue: { rgba: "rgba(0,124,255,#{opacity})", hex: '#007CFF' },
-      indigo: { rgba: "rgba(92,92,235,#{opacity})", hex: '#5C5CEB' },
-      purple: { rgba: "rgba(140,75,251,#{opacity})", hex: '#8C4BFB' },
-      pink: { rgba: "rgba(255,95,132,#{opacity})", hex: '#FF5F84' },
-      red: { rgba: "rgba(245,47,46,#{opacity})", hex: '#F52F2E' },
-      orange: { rgba: "rgba(254,146,26,#{opacity})", hex: '#FE921A' },
-      yellow: { rgba: "rgba(255,214,19,#{opacity})", hex: '#FFD613' }
+      lime: { rgba: "rgba(221,234,76,#{opacity})", hex: '#DDEA4C' },
+      green: { rgba: "rgba(109,202,150,#{opacity})", hex: '#6DCA96' },
+      teal: { rgba: "rgba(86,210,238,#{opacity})", hex: '#56D2EE' },
+      blue: { rgba: "rgba(31,140,235,#{opacity})", hex: '#1F8CEB' },
+      indigo: { rgba: "rgba(126,97,234,#{opacity})", hex: '#7E61EA' },
+      purple: { rgba: "rgba(154,111,199,#{opacity})", hex: '#9A6FC7' },
+      pink: { rgba: "rgba(255,127,127,#{opacity})", hex: '#FF7F7F' },
+      red: { rgba: "rgba(224,78,80,#{opacity})", hex: '#E04E50' },
+      orange: { rgba: "rgba(255,144,78,#{opacity})", hex: '#FF904E' },
+      yellow: { rgba: "rgba(242,195,48,#{opacity})", hex: '#F2C330' }
     }
   end
 
--- vendor/assets/javascripts/base/_colorpicker.js
+++ vendor/assets/javascripts/base/_colorpicker.js
@@ -18,9 +18,9 @@
   Colorpicker.DEFAULTS = {
     callback: function (color) {},
     colors: [
-      '#ABD026', '#72C914', '#32C9EA', '#007CFF', '#5C5CEB',
-      '#8C4BFB', '#FF5F84', '#F52F2E', '#FE921A', '#FFD613',
-      '#273448', '#8492A6', '#ECEFF5', '#FFFFFF'
+      '#DDEA4C', '#6DCA96', '#56D2EE', '#1F8CEB', '#7E61EA',
+      '#9A6FC7', '#FF7F7F', '#E04E50', '#FF904E', '#F2C330',
+      '#343B4A', '#9BA7BA', '#ECEFF5', '#FFFFFF'
     ],
     item: '<li><button type="button"></button></li>',
     menu: '<ul class="colorpicker dropmenu caret"></ul>'

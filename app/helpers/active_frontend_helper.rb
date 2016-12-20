aa7bdb7b343e97789cf6d5d3b9d0aec3ffca4e36
Juan Gomez <j.gomez@drexed.com>
2016-12-20 15:42:35 -0500

    updates

--- app/helpers/active_frontend_helper.rb
+++ app/helpers/active_frontend_helper.rb
@@ -3,26 +3,26 @@ module ActiveFrontendHelper
   def colors(opacity = 1)
     {
       transparent: { rgba: "rgba(0,0,0,0)", hex: '#FFFFFF' },
-      dark_black: { rgba: "rgba(32,42,58,#{opacity})", hex: '#202A3A' },
-      black: { rgba: "rgba(39,52,68,#{opacity})", hex: '#273444' },
-      light_black: { rgba: "rgba(48,64,80,#{opacity})", hex: '#304050' },
-      dark_gray: { rgba: "rgba(108,126,144,#{opacity})",  hex: '#6C7E90' },
+      dark_black: { rgba: "rgba(47,59,72,#{opacity})", hex: '#2F3B48' },
+      black: { rgba: "rgba(54,68,83,#{opacity})", hex: '#364453' },
+      light_black: { rgba: "rgba(61,77,93,#{opacity})", hex: '#3D4D5D' },
+      dark_gray: { rgba: "rgba(110,128,150,#{opacity})",  hex: '#6E8096' },
       gray: { rgba: "rgba(130,146,164,#{opacity})", hex: '#8292A4' },
-      light_gray: { rgba: "rgba(153,166,181,#{opacity})", hex: '#99A6B5' },
-      dark_haze: { rgba: "rgba(228,231,238,#{opacity})", hex: '#E4E7EE' },
+      light_gray: { rgba: "rgba(153,166,183,#{opacity})", hex: '#99A6B7' },
+      dark_haze: { rgba: "rgba(229,232,241,#{opacity})", hex: '#E5E8F1' },
       haze: { rgba: "rgba(238,240,246,#{opacity})", hex: '#EEF0F6' },
       light_haze: { rgba: "rgba(249,250,252,#{opacity})", hex: '#F9FAFC' },
       white: { rgba: "rgba(255,255,255,#{opacity})", hex: '#FFFFFF' },
-      lime: { rgba: "rgba(154,191,91,#{opacity})", hex: '#9ABF5B' },
-      green: { rgba: "rgba(50,194,81,#{opacity})", hex: '#32C251' },
-      teal: { rgba: "rgba(55,186,212,#{opacity})", hex: '#37BAD4' },
-      blue: { rgba: "rgba(36,126,236,#{opacity})", hex: '#247EEC' },
-      indigo: { rgba: "rgba(126,97,234,#{opacity})", hex: '#7E61EA' },
-      purple: { rgba: "rgba(173,101,228,#{opacity})", hex: '#AD65E4' },
-      pink: { rgba: "rgba(241,93,121,#{opacity})", hex: '#F15D79' },
-      red: { rgba: "rgba(221,61,63,#{opacity})", hex: '#DD3D3F' },
-      orange: { rgba: "rgba(234,133,50,#{opacity})", hex: '#EA8532' },
-      yellow: { rgba: "rgba(242,203,36,#{opacity})", hex: '#F2CB24' }
+      lime: { rgba: "rgba(161,215,77,#{opacity})", hex: '#A1D74D' },
+      green: { rgba: "rgba(46,204,64,#{opacity})", hex: '#2ECC40' },
+      teal: { rgba: "rgba(46,207,221,#{opacity})", hex: '#2ECFDD' },
+      blue: { rgba: "rgba(13,134,246,#{opacity})", hex: '#0D86F6' },
+      indigo: { rgba: "rgba(126,99,226,#{opacity})", hex: '#7E63E2' },
+      purple: { rgba: "rgba(186,91,206,#{opacity})", hex: '#BA5BCE' },
+      pink: { rgba: "rgba(239,76,136,#{opacity})", hex: '#EA4C88' },
+      red: { rgba: "rgba(235,70,34,#{opacity})", hex: '#EB4622' },
+      orange: { rgba: "rgba(253,163,13,#{opacity})", hex: '#FDA30D' },
+      yellow: { rgba: "rgba(246,209,35,#{opacity})", hex: '#F6D123' }
     }
   end
 
--- lib/active_frontend/version.rb
+++ lib/active_frontend/version.rb
@@ -1,3 +1,3 @@
 module ActiveFrontend
-  VERSION = '14.0.40'.freeze
+  VERSION = '14.0.41'.freeze
 end
